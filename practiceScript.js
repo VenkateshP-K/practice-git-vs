821f64110274f54a49064922f411126968b51f60
Venkatesh <venkateshpk06@gmail.com>
2024-02-07 09:05:10 +0530

    update

--- practiceScript.js
+++ practiceScript.js
@@ -5,8 +5,16 @@ request.send()//getting files from api
 request.onload = function(){
     var res = JSON.parse(request.response)//api given data as string.use parse to get data as json object
     console.log(res)
-}
-for (var i=0;i<res.length;i++){
-    console.log(res[i].status)
-}
 
+    var pop = res.filter((ele)=>ele.population<100000);
+   console.log(pop);
+
+   var countries_details = res.forEach(
+    (country) => console.log(`${country.name.common},
+   ${country.capital}, 
+   ${country.flag}`))
+
+   var total_pop = res.reduce((pop_res, country) => pop_res +country.population, 0)
+   console.log("total population : "+total_pop)
+ 
+}
