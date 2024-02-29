ceff1b38f5a5712720c5acbb440988e3e8083165
Venkatesh <venkateshpk06@gmail.com>
2024-03-01 00:01:47 +0530

    ud

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
+/*function create_th(tagname,attrname,attrvalue,content){
+   var ele = document.createElement(tagname)
+   ele.setAttribute(attrname,attrvalue)
+   ele.innerHTML=content;
+   return ele;
+}
+var thead = document.createElement("thead");
+var tr = document.createElement("tr")
+var th1 = create_th("th","scope","col","ID");
+var th2 = create_th("th","scope","col","NAME");
+var th3 = create_th("th","scope","col","EMAIL");
+thead.append(tr,th1,th2,th3);
+*/
 async function foo(){
     var res = await fetch("https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json")
     var res02 = await res.json();
     console.log (res02);
 
+    for(var i=0;i<=res02.length;i++){
     var div = document.createElement("div");
-    div.innerHTML=`<nav aria-label="Page navigation example">
-    <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-          <span aria-hidden="true">&raquo;</span>
-        </a>
-      </li>
-    </ul>
-  </nav>`
-
+    div.innerHTML=`<table class="table table-dark">
+    <tbody>
+      <tr>
+        <td>${res02[i].id}</td>
+        <td>${res02[i].name}</td>
+        <td>${res02[i].email}</td>
+      </tr>
+    </tbody>
+  </table>`
 document.body.append(div);
+}
 }
 foo();
